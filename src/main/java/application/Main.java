099a262aa022cac01ce2063b22c8de68d9511d79
viv <vivin.abraham@ibm.com>
2020-06-02 11:39:15 -0500

    first commit

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ibm.javametrics.spring", "application.resources", 
+								"application.health", "application"})
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
